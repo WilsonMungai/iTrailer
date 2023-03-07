5bdad88de3794a7de8e4114b16618ff4e8939ccc
WilsonMungai <wilsonmuguthi@gmail.com>
2023-03-07 10:07:48 -0500

    project setup

--- iTrailers/Controllers/TabBarViewController.swift
+++ iTrailers/Controllers/TabBarViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TabBarViewController.swift
 //  iTrailers
 //
 //  Created by Wilson Mungai on 2023-03-07.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemRed
     }
 
-
 }
-

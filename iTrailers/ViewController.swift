73796ccb9286e0666520d891a6ed9b2605a41266
WilsonMungai <wilsonmuguthi@gmail.com>
2023-03-07 09:46:57 -0500

    Initial Commit

---
+++ iTrailers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTrailers
+//
+//  Created by Wilson Mungai on 2023-03-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

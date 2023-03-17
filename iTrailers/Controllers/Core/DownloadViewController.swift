20ff57261d119aed187bd18c949149e03a56ad45
WilsonMungai <wilsonmuguthi@gmail.com>
2023-03-17 18:54:25 -0400

    added download table view

--- iTrailers/Controllers/Core/DownloadViewController.swift
+++ iTrailers/Controllers/Core/DownloadViewController.swift
@@ -1,1 +1,59 @@
+//
+//  DownloadViewController.swift
+//  iTrailers
+//
+//  Created by Wilson Mungai on 2023-03-07.
+//
 
+import UIKit
+
+class DownloadViewController: UIViewController {
+    
+    let downloadTabelView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.register(PosterTableViewCell.self, forCellReuseIdentifier: PosterTableViewCell.cellIdentifier)
+        tableView.backgroundColor = UIColor.clear
+        return tableView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(downloadTabelView)
+        tabelViewSetup()
+        title = "Download"
+        view.backgroundColor = .systemBackground
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        downloadTabelView.frame = view.bounds
+    }
+    
+    private func tabelViewSetup() {
+        downloadTabelView.delegate = self
+        downloadTabelView.dataSource = self
+    }
+    
+    
+}
+
+
+// MARK: - Table view extension
+extension DownloadViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: PosterTableViewCell.cellIdentifier, for: indexPath)
+//        cell.backgroundColor = .systemCyan
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 300
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}

62a01b75b58117ea56ac27c96896ac7ff09e3fbc
WilsonMungai <wilsonmuguthi@gmail.com>
2023-03-17 18:53:49 -0400

    added trending table view

--- iTrailers/Controllers/Core/TrendingViewController.swift
+++ iTrailers/Controllers/Core/TrendingViewController.swift
@@ -1,1 +1,58 @@
+//
+//  TrendingViewController.swift
+//  iTrailers
+//
+//  Created by Wilson Mungai on 2023-03-07.
+//
 
+import UIKit
+
+class TrendingViewController: UIViewController {
+    
+    // MARK: - UI component
+    let trendingTableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.register(PosterTableViewCell.self, forCellReuseIdentifier: PosterTableViewCell.cellIdentifier)
+        tableView.separatorColor = UIColor.clear
+        return tableView
+    }()
+    
+    // MARK: - Lifecycle methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(trendingTableView)
+        title = "Trending"
+        view.backgroundColor = .systemBackground
+        tabelViewSetup()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        trendingTableView.frame = view.bounds
+    }
+    
+    private func tabelViewSetup() {
+        trendingTableView.delegate = self
+        trendingTableView.dataSource = self
+    }
+}
+
+// MARK: - Table view extension
+extension TrendingViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: PosterTableViewCell.cellIdentifier, for: indexPath)
+//        cell.backgroundColor = .systemCyan
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 300
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
